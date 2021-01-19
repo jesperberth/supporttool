8a61924d0cddee230a79bf6dac28134caabce376
Jesper Berth <jesper.berth@arrow.com>
2021-01-19 13:19:45 +0100

    change let windows manage your default print

--- fileprint.ps1
+++ fileprint.ps1
@@ -28,6 +28,8 @@ write-host "setup printers"
 add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 Color"
 add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 SH"
 
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value ”1”
+
 (Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "\\server2.jjk.local\Canon C3730i PCL6 SH").SetDefaultPrinter()
 
 }

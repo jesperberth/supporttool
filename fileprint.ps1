487272c3d9bbec7da3f9400d1f0e55282c02696d
Jesper Berth <jesper.berth@arrow.com>
2021-01-20 14:16:16 +0100

    change fileprint to workgroup

--- fileprint.ps1
+++ fileprint.ps1
@@ -1,35 +1,35 @@
-﻿$testsc = Get-StoredCredential -AsCredentialObject -Target "server2.jjk.local"
+﻿$testsc = Get-StoredCredential -AsCredentialObject -Target "server2"
 
 if($testsc -ne $null){
     write-host "Exist"
 }
 else{
     write-host "Dosnt exist"
-    write-host -ForegroundColor Green "Please enter credentials for Server2.jjk.local`n"
+    write-host -ForegroundColor Green "Please enter credentials for Server2`n"
     write-host -ForegroundColor Yellow "Format: <username>"
     $creds = Get-Credential
-    New-StoredCredential -Credentials $creds -Persist Enterprise -Type Generic -Target "server2.jjk.local"
+    New-StoredCredential -Credentials $creds -Persist Enterprise -Type Generic -Target "server2"
 }
-$creds2 = Get-StoredCredential -Target "server2.jjk.local"
+$creds2 = Get-StoredCredential -Target "server2"
 
 if(Get-PSDrive -name "J" -ErrorAction SilentlyContinue){
 Write-Host "Drives Exist"
 }
 else{
 write-host "setup drives"
-New-PSDrive -Name "J" -Root "\\server2.jjk.local\JJK" -Persist -PSProvider "FileSystem" -Credential $creds2
+New-PSDrive -Name "J" -Root "\\server2\JJK" -Persist -PSProvider "FileSystem" -Credential $creds2
 }
 
-if(Get-Printer -name "\\server2.jjk.local\Canon C3730i PCL6 SH"-ErrorAction SilentlyContinue){
+if(Get-Printer -name "\\server2\Canon C3730i PCL6 SH"-ErrorAction SilentlyContinue){
 Write-Host "Printer Exist"
 }
 else{
 write-host "setup printers"
-add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 Color"
-add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 SH"
+add-printer -ConnectionName "\\server2\Canon C3730i PCL6 Color"
+add-printer -ConnectionName "\\server2\Canon C3730i PCL6 SH"
 
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value ”1”
 
-(Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "\\server2.jjk.local\Canon C3730i PCL6 SH").SetDefaultPrinter()
+(Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "\\server2\Canon C3730i PCL6 SH").SetDefaultPrinter()
 
 }

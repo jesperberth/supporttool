892e5a9c3c9c04a3ebe965a662c8a4a7f2aefbe7
Jesper Berth <jesper.berth@arrow.com>
2021-10-28 12:40:50 +0200

    Added new local printer

--- fileprint.ps1
+++ fileprint.ps1
@@ -22,23 +22,27 @@ net use J: /delete /y
 New-PSDrive -Name "J" -Root "\\server2.jjk.local\JJK" -Persist -PSProvider "FileSystem" -Credential $creds2
 }
 
-if(Get-Printer -name "\\server2.jjk.local\Canon C3730i PCL6 SH"-ErrorAction SilentlyContinue){
+if(Get-Printer -name "Canon C3730i PCL6 SH"-ErrorAction SilentlyContinue){
 Write-Host "Printer Canon C3730i PCL6 SH exist"
 }
 else{
-write-host "setup printer - Canon C3730i PCL6 SH"
-add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 SH"
-
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" -Name "LegacyDefaultPrinterMode" -Value ”1”
-
-(Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "\\server2.jjk.local\Canon C3730i PCL6 SH").SetDefaultPrinter()
-
+    write-host "setup printer - Canon C3730i PCL6 SH"
+    Add-PrinterPort -Name "CanonIP-SH" -PrinterHostAddress "192.168.1.12"
+    pnputil.exe -a "C:\Temp\Driver\CNP60MA64.INF"
+    Add-PrinterDriver -Name "Canon Generic Plus PCL6"
+    Add-Printer -Name "Canon C3730i PCL6 SH"  -PortName "CanonIP-SH" -DriverName "Canon Generic Plus PCL6"
+    printui.exe /Sr /n "Canon C3730i PCL6 SH" /a "C:\Temp\Canon_C3730i_PCL6_SH.dat" c d g r p
+    (Get-WMIObject -ClassName win32_printer |Where-Object -Property Name -eq "Canon C3730i PCL6 SH").SetDefaultPrinter()
 }
 
-if(Get-Printer -name "\\server2.jjk.local\Canon C3730i PCL6 Color"-ErrorAction SilentlyContinue){
+if(Get-Printer -name "Canon C3730i PCL6 Color"-ErrorAction SilentlyContinue){
     Write-Host "Printer Canon C3730i PCL6 Color exist"
     }
     else{
-    write-host "setup printer - Canon C3730i PCL6 Color"
-    add-printer -ConnectionName "\\server2.jjk.local\Canon C3730i PCL6 Color"
+        write-host "setup printer - Canon C3730i PCL6 Color"
+        Add-PrinterPort -Name "CanonIP-Color" -PrinterHostAddress "192.168.1.12"
+        pnputil.exe -a "C:\Temp\Driver\CNP60MA64.INF"
+        Add-PrinterDriver -Name "Canon Generic Plus PCL6"
+        Add-Printer -Name "Canon C3730i PCL6 Color"  -PortName "CanonIP-Color" -DriverName "Canon Generic Plus PCL6"
+        printui.exe /Sr /n "Canon C3730i PCL6 Color" /a "C:\Temp\Canon_C3730i_PCL6_Color.dat" c d g r p
     }

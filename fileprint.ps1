5f5c40bf03bd20d3adf09fec5a4ba9cbeb06b360
Jesper Berth <jesper.berth@arrow.com>
2022-06-07 08:41:45 +0200

    changed servername

--- fileprint.ps1
+++ fileprint.ps1
@@ -1,17 +1,17 @@
-﻿$testsc = Get-StoredCredential -Target "server2.jjk.local" -Type DomainVisiblePassword
+﻿$testsc = Get-StoredCredential -Target "server2" -Type DomainVisiblePassword
 
 if($testsc){
-    write-host "Credentials for server2.jjk.local"
+    write-host "Credentials for server2"
 }
 else{
     write-host "No Credentials"
-    write-host -ForegroundColor Green "Please enter credentials for Server2.jjk.local`n"
+    write-host -ForegroundColor Green "Please enter credentials for Server2`n"
     write-host -ForegroundColor Yellow "Format: <username>"
     $creds = Get-Credential
-    New-StoredCredential -Credentials $creds -Persist Enterprise -Type DomainVisiblePassword -Target "server2.jjk.local"
+    New-StoredCredential -Credentials $creds -Persist Enterprise -Type DomainVisiblePassword -Target "server2"
     }
 
-$creds2 = Get-StoredCredential -Target "server2.jjk.local" -Type DomainVisiblePassword
+$creds2 = Get-StoredCredential -Target "server2" -Type DomainVisiblePassword
 
 if(Get-PSDrive -name "J" -ErrorAction SilentlyContinue){
 Write-Host "Drives Exist"
@@ -19,7 +19,7 @@ Write-Host "Drives Exist"
 else{
 write-host "setup drives"
 net use J: /delete /y
-New-PSDrive -Name "J" -Root "\\server2.jjk.local\JJK" -Persist -PSProvider "FileSystem" -Credential $creds2
+New-PSDrive -Name "J" -Root "\\server2\JJK" -Persist -PSProvider "FileSystem" -Credential $creds2
 }
 
 if(Get-Printer -name "Canon C3730i PCL6 SH"-ErrorAction SilentlyContinue){
